78927a4590f4b78893149ac25afb82a2e7dfe452
LaisDourado <lais.santos_dourado@hotmail.com>
2023-05-15 17:56:40 -0300

    Obtendo um usuário por id

--- src/main/java/com/laisdourado/testejava/services/PessoaService.java
+++ src/main/java/com/laisdourado/testejava/services/PessoaService.java
@@ -1,12 +1,14 @@
 package com.laisdourado.testejava.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.laisdourado.testejava.domain.Pessoa;
 import com.laisdourado.testejava.repository.PessoaRepository;
+import com.laisdourado.testejava.services.exception.ObjectNotFoundException;
 
 @Service
 public class PessoaService {
@@ -16,4 +18,10 @@ public class PessoaService {
 	public List<Pessoa> findAll(){
 		return repo.findAll();
 	}
+	
+	public Pessoa findById(String id) {
+		Optional<Pessoa> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
+
